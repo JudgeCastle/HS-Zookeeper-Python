40dbf1492c68a3717e4ceb3f962c3b8e27a13a23
JudgeCastle <56881291+JudgeCastle@users.noreply.github.com>
2024-07-17 16:27:40 -0400

    The course work
    This is a copy of the course work.

---
+++ Topics/Declaring functions/Captain/main.py
@@ -0,0 +1,8 @@
+name = "Jack Sparrow"
+# declare your function here
+def captain_adder(name):
+    if "captain " in name:
+        print(name)
+    else:
+        name = "captain " + name
+        print(name)
